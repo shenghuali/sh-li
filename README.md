111fce4a7f288bc5622db94cc7999f07ec001793
Shenghua Li <shengli_1031@hotmail.com>
2018-08-23 14:34:32 +0800

    UPDATE

--- README.md
+++
@@ -1,9 +0,0 @@
-# sh-li
-
-
-Here is an example of code:
-```bash	
-	<div>
-	example
-	</div>
-```
--- my-app/config/config.js
+++
@@ -1,6 +0,0 @@
-var config ={
-    cdn:"", //cdn address
-    name:"my-app", //project name
-    width: 375 ,
-}
-module.exports = config
